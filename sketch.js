2e5e36f831b5023e8f23c24e31d40298913f7191
gorjoha <93801364+gorjoha@users.noreply.github.com>
2022-01-11 16:49:22 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,98 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+
+//crea aquí las variables feed y lastFed
+var feed;
+var lastFed; 
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //crea aquí el boton Alimentar al perro
+
+  feed=createButton("Alimenta al perro");//no se si sea feed la variable correcta
+  feed.position(950,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Agregar Alimento");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //escribe el código para leer el valor de tiempo de alimentación de la base de datos
+  
+ 
+  //escribe el código para mostrar el texto lastFed time aquí
+  fill("white");
+  textSize(20);
+  text("Última hora en que se alimentó : 9 PM",240,30);
+ 
+  drawSprites();
+}
+
+//función para leer la Existencia de alimento
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+  if(lastFed>=12){
+    //la hora en formato PM cuando lastFed sea mayor que 12
+  }else if(lastFed==0){
+    text("Última hora en que se alimentó : 12 PM",350,30);
+  }else{
+    //muestra la hora en formato AM cuando lastFed sea mayor que 12
+  }
+
+  var food_stock_val=foodObj.getFoodStock();
+  if(food_stock_val<=0){
+    foodObj.updateFoodStock(food_stock_val*0);
+  }else{
+    //foodObj.updateFoodStock(food_stock_val-1);
+    foodS--;
+    database.ref('/').update({
+      Food:foodS
+    })
+  }
+
+  if(foodObj.getFoodStock()<= 0){
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  }
+  //escribe el código aquí para actualizar las existencia de alimento, y la última vez que se alimentó al perro
+}
+
+//función para agregar alimento al almacén
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
